b17c3ed70db3c62508220c53fedc746935a8950c
Sathvik <sathvikachanta003@gmail.com>
2022-10-23 13:12:29 +0530

    first commit

---
+++ src/components/Player.js
@@ -0,0 +1,28 @@
+import { useFrame, useThree } from "@react-three/fiber"
+import { useSphere } from "@react-three/cannon";
+import { Vector3 } from "three";
+import { useEffect, useRef } from "react";
+
+export const Player = () => {
+    const { camera } = useThree();
+    const [ref, api] = useSphere(() => ({
+        mass: 1,
+        type: 'Dynamic',
+        position: [0, 0, 0]
+    }))
+
+    const pos = useRef([0, 0, 0])
+    useEffect(() => {
+        api.position.subscribe((p) => pos.current = p)
+    }, [api.position])
+
+    useFrame(() => {
+        camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
+    })
+
+    return (
+        <mesh ref={ref}>
+
+        </mesh>
+    )
+}
